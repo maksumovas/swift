5a7fdd77776e3f2cc1664bb0038bd89dcc407af9
maksumovas <118979305+maksumovas@users.noreply.github.com>
2022-11-24 18:46:41 +0600

    dz3_2

---
+++ main_1.swift
@@ -0,0 +1,53 @@
+//
+//  main.swift
+//  dz 3_2
+//
+//  Created by Nika Abdulakhatova on 24.11.2022.
+//
+
+import Foundation
+
+func dateConverter(date: Int){
+    switch date {
+    case 1:
+        print("jan")
+        break
+    case 2:
+        print("feb")
+        break
+    case 3:
+        print("mar")
+        break
+    case 4:
+        print("apr")
+        break
+    case 5:
+        print("may")
+        break
+    case 6:
+        print("jun")
+        break
+    case 7:
+        print("july")
+        break
+    case 8:
+        print("aug")
+        break
+    case 9:
+        print("sep")
+        break
+    case 10:
+        print("oct")
+        break
+    case 11:
+        print("nov")
+        break
+    case 12:
+        print("dec")
+        break
+        
+    default: break
+ 
+    }
+}
+dateConverter(date: 1)
