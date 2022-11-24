2c3e667ffa9320a8052723d79bb800b367e7627f
maksumovas <118979305+maksumovas@users.noreply.github.com>
2022-11-24 20:58:42 +0600

    Update main.swift

--- main.swift
+++ main.swift
@@ -9,36 +9,36 @@
 
 func genderDetect(age: Int, city: String){
     if(age >= 0 && age <= 17 && city == "Bishkek"){
-        print("child")
+        print("ребенок кыргыз")
     }
     else if(age >= 18 && age <= 50 && city == "Bishkek"){
         print("Кыргыз среднего возраста")
     }
     else if(age >= 55 && age < 70 && city == "Bishkek"){
-        print("пенсионер")
+        print("пенсионер кыргыз")
     }
 
  
     
     if(age >= 0 && age <= 17 && city == "Madrid"){
-        print("child")
+        print("ребенок испанец"")
     }
     else if(age >= 18 && age <= 50 && city == "Madrid"){
         print("Испанец среднего возраста")
     }
     else if(age >= 55 && age < 70 && city == "Madrid"){
-        print("песионер")
+        print("песионер испанец")
     }
 
    
     if(age >= 0 && age <= 17 && city == "London"){
-        print("child")
+        print("ребенок англичанин"")
     }
     else if(age >= 18 && age <= 50 && city == "London"){
-        print("Англичан среднего возраста")
+        print("Англичанин среднего возраста")
     }
     else if(age >= 55 && age < 70 && city == "London"){
-        print("песионер")
+        print("песионер англичанин")
     }
 
     
