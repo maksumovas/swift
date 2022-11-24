278dc1e9f1fd32cab132f2d42b334a8b7f941f55
maksumovas <118979305+maksumovas@users.noreply.github.com>
2022-11-24 20:34:52 +0600

    Update main.swift

--- main.swift
+++ main.swift
@@ -11,45 +11,40 @@ func genderDetect(age: Int, city: String){
     if(age >= 0 && age <= 17 && city == "Bishkek"){
         print("child")
     }
-    else if(age >= 18 && age <= 30 && city == "Bishkek"){
-        print("adult")
+    else if(age >= 18 && age <= 50 && city == "Bishkek"){
+        print("Кыргыз среднего возраста")
     }
     else if(age >= 55 && age < 70 && city == "Bishkek"){
         print("пенсионер")
     }
-//    else{
-//        print("долгожитель")
-//    }
+
+ 
     
-    if(age >= 0 && age <= 17 && city == "Tash"){
+    if(age >= 0 && age <= 17 && city == "Madrid"){
         print("child")
     }
-    else if(age >= 18 && age <= 50 && city == "Tashkent"){
-        print("adult")
+    else if(age >= 18 && age <= 50 && city == "Madrid"){
+        print("Испанец среднего возраста")
     }
-    else if(age >= 55 && age < 70 && city == "Tashkent"){
+    else if(age >= 55 && age < 70 && city == "Madrid"){
         print("песионер")
     }
-//    else{
-//        print("долгожитель")
-//    }
+
+   
     if(age >= 0 && age <= 17 && city == "London"){
         print("child")
     }
     else if(age >= 18 && age <= 50 && city == "London"){
-        print("adult")
+        print("Англичан среднего возраста")
     }
     else if(age >= 55 && age < 70 && city == "London"){
         print("песионер")
     }
-//    else{
-//        print("долгожитель")
-//    }
 
     
 }
 
 
 genderDetect(age:17, city: "Bishkek")
-genderDetect(age:22, city: "Tashkent")
+genderDetect(age:22, city: "Madrid")
 genderDetect(age:57, city: "London")
