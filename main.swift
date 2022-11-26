c52e4d2664f698474e522918960f6012defc76f9
maksumovas <118979305+maksumovas@users.noreply.github.com>
2022-11-27 00:21:17 +0600

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,50 +1,14 @@
-//
-//  main.swift
-//  dz3
-//
-//  Created by Sofia Maksumova on 24.11.2022.
-//
+import Foundation
 
+let  words = ["tv", "tea", "apple", "table", "people", "journey", "mountain", "strengths", "conscience", "performance", "announcment", "objectivizing", "accidentalness", "methoxybenzenes", "ACCLIMATIZATIONS", "administratorship", "ABSENTMINDEDNESSES", "ABDOMINOHYSTEROTOMY","ABDOMINOHYSTERECTOMY"]
+var number = 0
 
-
-func genderDetect(age: Int, city: String){
-    if(age >= 0 && age <= 17 && city == "Bishkek"){
-        print("ребенок кыргыз")
-    }
-    else if(age >= 18 && age <= 50 && city == "Bishkek"){
-        print("Кыргыз среднего возраста")
-    }
-    else if(age >= 55 && age < 70 && city == "Bishkek"){
-        print("пенсионер кыргыз")
-    }
-
- 
-    
-    if(age >= 0 && age <= 17 && city == "Madrid"){
-        print("ребенок испанец")
-    }
-    else if(age >= 18 && age <= 50 && city == "Madrid"){
-        print("Испанец среднего возраста")
-    }
-    else if(age >= 55 && age < 70 && city == "Madrid"){
-        print("песионер испанец")
-    }
-
-   
-    if(age >= 0 && age <= 17 && city == "London"){
-        print("ребенок англичанин")
-    }
-    else if(age >= 18 && age <= 50 && city == "London"){
-        print("Англичанин среднего возраста")
-    }
-    else if(age >= 55 && age < 70 && city == "London"){
-        print("песионер англичанин")
-    }
-
-    
+func countChars(w: String) -> Int{
+    return w.count
 }
 
-
-genderDetect(age:17, city: "Bishkek")
-genderDetect(age:22, city: "Madrid")
-genderDetect(age:57, city: "London")
+for word in words {
+    print("\(word) - \(countChars(w:word)) символов")
+   
+}
+          
