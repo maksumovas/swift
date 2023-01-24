e45298bd5842e1c00890319f3d89276c11b435c0
maksumovas <118979305+maksumovas@users.noreply.github.com>
2023-01-24 13:25:09 +0600

    Update main_1.swift

--- main_1.swift
+++ main_1.swift
@@ -1,64 +1,56 @@
+//
 //  main.swift
-//  dz 3_2
+//  4hw_2month_23.1.23
 //
-//  Created by Sofia Maksumova on 24.11.2022.
+//  Created by Maksumova Sofia on 24/1/23.
 //
+
 import Foundation
 
-func dateConverter(date: Int){
-    switch date {
-    case 1:
-        print("jan")
-        print ("Месяц 1 - Январь. Зима. 7 января -   Рождество Христово")
-        break
-    case 2:
-        print("feb")
-        print ("Месяц 2 - Февраль. Зима. 14 февраля - День Святого Валентина")
-        break
-    case 3:
-        print("mar")
-        print ("Месяц 3 - Март. Весна. 8 марта - Международный женский день")
-        break
-    case 4:
-        print("apr")
-        print ("Месяц 4 - Апрель. Весна. 21 апреля - День Космонавтики")
-        break
-    case 5:
-        print("may")
-        print ("Месяц 5 - Май. Весна. 9 мая - День Победы")
-        break
-    case 6:
-        print("jun")
-        print ("1")
-        print ("Месяц 6 - Июнь. Лето. 1 июня - Международный день защиты детей")
-        break
-    case 7:
-        print("july")
-        print ("Месяц 7 - Июль. Лето. 5 июля - День трудоголиков")
-        break
-    case 8:
-        print("aug")
-        print ("Месяц 8 - Август. Лето. 31 августа - День независимости КР")
-        break
-    case 9:
-        print("sep")
-        print ("Месяц 9 - Сентябрь. Осень. 1 Сентября - День знаний")
-        break
-    case 10:
-        print("oct")
-        print ("Месяц 10 - Октябрь. Осень. 5 октября - День учителя")
-        break
-    case 11:
-        print("nov")
-        print ("Месяц 11 - Ноябрь. Осень. 1 Ноября - Международный день вегана")
-        break
-    case 12:
-        print("dec")
-        print ("Месяц 12 - Декабрь. Зима. 31 декабря - Новый Год")
-        break
-        
-    default: break
- 
+//Создать класс Пенсионер с параметрами имя, фамилия, текущая сумма пенсии, кол-во лет на пенсии. Создать объекты в main и заполнить данными. Создать класс Соц.Фонд с приватным параметром массив пенсионеров, также публичная функция добавить пенсионера, приватная функция, которая увеличивает пенсию если кол-во лет на пенсии превышает 5 лет на 1000 сом. Эта приватная функция вызывается в другой доступной функции раздать пенсию. Функция раздачи пенсии отображает всех пенсионеров: фио и сумма пенсии.
+
+class Pensioner{
+    var name: String
+    var surname: String
+    var pensionAmount: Int
+    var yearsOnPension: Double
+    init(name: String, surname: String, pensionAmount: Int, yearsOnPension: Double) {
+        self.name = name
+        self.surname = surname
+        self.pensionAmount = pensionAmount
+        self.yearsOnPension = yearsOnPension
     }
 }
-dateConverter(date: 12)
+class socialFund{
+    private var pensioners = [Pensioner]()
+    public func addPensioner(pensioner: Pensioner){
+        pensioners.append(pensioner)
+    }
+    private func increasePension(){
+        for pensioner in pensioners {
+            if pensioner.yearsOnPension > 5{
+                pensioner.pensionAmount += 1000
+            }
+        }
+    }
+        
+    func distributePension() {
+     for pensioner in pensioners {
+         print("\(pensioner.name) \(pensioner.surname) has a pension of \(pensioner.pensionAmount)")
+            }
+        }
+    }
+
+var pensioner1 = Pensioner(name: "Marie", surname: "Hackenberg", pensionAmount: 5000, yearsOnPension: 10)
+var pensioner2 = Pensioner(name: "Boris", surname: "Wilson", pensionAmount: 2500, yearsOnPension: 3)
+
+let SocialFund = socialFund()
+SocialFund.addPensioner(pensioner: pensioner1)
+SocialFund.addPensioner(pensioner: pensioner2)
+SocialFund.distributePension()
+
+      
+
+
+
+
